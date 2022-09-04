ca3b818c27824851d139ccf3f6bedff29a0f39c1
Kiki Widianto <68271685+kikiwidianto@users.noreply.github.com>
2022-09-04 22:51:20 +0700

    Add files via upload

---
+++ k-test-script-orangehrm-buzz.py
@@ -0,0 +1,37 @@
+import unittest
+import time
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+class TestLogin(unittest.TestCase):  # TEST SCENARIO
+
+    def setUp(self): 
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        
+    def test_a_access_buzz(self): 
+        # steps
+        browser = self.browser #buka web browser
+        browser.get("https://opensource-demo.orangehrmlive.com/") # buka situs
+        time.sleep(8)
+        browser.find_element(By.NAME,"username").send_keys("Admin") # isi username
+        time.sleep(1)
+        browser.find_element(By.NAME,"password").send_keys("admin123") # isi password
+        time.sleep(1)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button").click() # klik tombol sign in
+        time.sleep(8)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[11]/a/span").click() # klik menu Buzz
+        time.sleep(5)
+        
+        # validasi
+        text_atas = browser.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/header/div[1]/div[1]/span/h6").text
+        text_bawah = browser.find_element(By.XPATH,"/html/body/div/div[1]/div[2]/div[2]/div/div/div/h4").text
+
+        self.assertIn('Buzz', text_atas)
+        self.assertEqual(text_bawah, 'Launching Soon')
+
+    def tearDown(self): 
+        self.browser.close() 
+
+if __name__ == "__main__": 
+    unittest.main()
